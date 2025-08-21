b4275113c241329cdf360f7db241d485deec6e6d
Camilo Seguro <82466584+CamiloSeguro@users.noreply.github.com>
2025-08-21 07:37:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,6 @@ st.header("Hola")
 st.write("Mundo")
 image = Image.open('imagen.jpg')
 st.image(image, caption='mundo')
+
+texto = st.text_input('escriba', 'ese es el texto')
+st.write('el texto es', texto)
