d47d051c446f94b8d4639649df6a37a4970fa40d
Camilo Seguro <82466584+CamiloSeguro@users.noreply.github.com>
2025-09-04 07:09:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+text_to_audi_codpy.txt
 import streamlit as st
 import os
 import time
@@ -5,13 +6,11 @@ import glob
 import os
 from gtts import gTTS
 from PIL import Image
-import base64
 
-st.title("Conversión de Texto a Audio")
+st.title("Interfases Multimodales.")
 image = Image.open('imagen.jpg')
-st.image(image, width=350)
-with st.sidebar:
-    st.subheader("Esrcibe y/o selecciona texto para ser escuchado.")
+
+st.image(image, width=200)
 
 
 try:
@@ -19,31 +18,21 @@ try:
 except:
     pass
 
-st.subheader("Una pequeña Fábula.")
-st.write('¡Ay! -dijo el ratón-. El mundo se hace cada día más pequeño. Al principio era tan grande que le tenía miedo. '  
-         ' Corría y corría y por cierto que me alegraba ver esos muros, a diestra y siniestra, en la distancia. ' 
-         ' Pero esas paredes se estrechan tan rápido que me encuentro en el último cuarto y ahí en el rincón está '  
-         ' la trampa sobre la cual debo pasar. Todo lo que debes hacer es cambiar de rumbo dijo el gato...y se lo comió. ' 
-         '  '
-         ' Franz Kafka.'
-        
-        )
+st.subheader("Texto a audio.")
+st.write('Las interfaces de texto a audio son fundamentales en las interfaces multimodales ya que permiten '  
+         'una comunicación más accesible y natural, facilitando la inclusión de personas con discapacidades ' 
+         ' visuales y permitiendo la interacción en situaciones donde no es posible leer texto. Estas interfaces '  
+         ' también impulsan tecnologías emergentes como los asistentes de voz inteligentes, haciendo que la tecnología ' 
+         ' sea más accesible e intuitiva para todos los usuarios')
            
-st.markdown(f"Quieres escucharlo?, copia el texto")
-text = st.text_area("Ingrese El texto a escuchar.")
 
-tld='com'
-option_lang = st.selectbox(
-    "Selecciona el lenguaje",
-    ("Español", "English"))
-if option_lang=="Español" :
-    lg='es'
-if option_lang=="English" :
-    lg='en'
+text = st.text_input("Ingrese el texto.")
 
-def text_to_speech(text, tld,lg):
+tld="es"
+
+def text_to_speech(text, tld):
     
-    tts = gTTS(text,lang=lg) # tts = gTTS(text,'en', tld, slow=False)
+    tts = gTTS(text,"es", tld, slow=False)
     try:
         my_file_name = text[0:20]
     except:
@@ -54,32 +43,17 @@ def text_to_speech(text, tld,lg):
 
 #display_output_text = st.checkbox("Verifica el texto")
 
-if st.button("convertir a Audio"):
-     result, output_text = text_to_speech(text, 'com',lg)#'tld
-     audio_file = open(f"temp/{result}.mp3", "rb")
-     audio_bytes = audio_file.read()
-     st.markdown(f"## Tú audio:")
-     st.audio(audio_bytes, format="audio/mp3", start_time=0)
+if st.button("convertir"):
+    result, output_text = text_to_speech(text, tld)
+    audio_file = open(f"temp/{result}.mp3", "rb")
+    audio_bytes = audio_file.read()
+    st.markdown(f"## Tú audio:")
+    st.audio(audio_bytes, format="audio/mp3", start_time=0)
 
-     #if display_output_text:
-     
-     #st.write(f" {output_text}")
-    
-#if st.button("ElevenLAabs",key=2):
-#     from elevenlabs import play
-#     from elevenlabs.client import ElevenLabs
-#     client = ElevenLabs(api_key="a71bb432d643bbf80986c0cf0970d91a", # Defaults to ELEVEN_API_KEY)
-#     audio = client.generate(text=f" {output_text}",voice="Rachel",model="eleven_multilingual_v1")
-#     audio_file = open(f"temp/{audio}.mp3", "rb")
+    #if display_output_text:
+    st.markdown(f"## Texto en audio:")
+    st.write(f" {output_text}")
 
-     with open(f"temp/{result}.mp3", "rb") as f:
-         data = f.read()
-
-     def get_binary_file_downloader_html(bin_file, file_label='File'):
-        bin_str = base64.b64encode(data).decode()
-        href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
-        return href
-     st.markdown(get_binary_file_downloader_html("audio.mp3", file_label="Audio File"), unsafe_allow_html=True)
 
 def remove_files(n):
     mp3_files = glob.glob("temp/*mp3")
